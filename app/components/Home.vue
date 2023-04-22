d1ad2c2882c9bc8f2c0cfb04c7a566e84a0b7058
Evgeny Pospelov <pospelov.genya@yandex.ru>
2023-04-22 10:49:33 +0500

    init commit

---
+++ app/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="info">
+                <FormattedString>
+                    <Span class="fas" text.decode="&#xf135; "/>
+                    <Span :text="message"/>
+                </FormattedString>
+            </Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data(){
+
+    },
+    methods:{
+
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+</style>
